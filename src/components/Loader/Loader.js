1552707b7d3157d496379a90263ff952188de4fc
mshahzaib1629 <mshahzaib1629@gmail.com>
2022-07-15 07:59:30 +0500

    images added into public directory

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,19 @@
 import React from "react";
 import LoaderContainer from "../../containers/LoaderContainer";
-import Mehdi from "../../assets/images/Mehdi";
+import Mehdi from "../../assets/images/Mehdi.js";
 
 const Loader = () => {
-    return (
-        <LoaderContainer>
-            <Mehdi width={250} />
-        </LoaderContainer>
-    );
+  return (
+    <LoaderContainer>
+      {/* <Mehdi width={250} /> */}
+
+      <img
+        src="images/shahzaib-splash.gif"
+        alt="Shahzaib Minhas"
+        width={250}
+      />
+    </LoaderContainer>
+  );
 };
 
 export default Loader;
