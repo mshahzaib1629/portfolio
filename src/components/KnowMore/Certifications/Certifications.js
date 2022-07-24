de4d239110817ae750f097d18bb63dfe33b0e31b
mshahzaib1629 <mshahzaib1629@gmail.com>
2022-07-24 16:40:54 +0500

    Pagination added in certifications

--- src/components/KnowMore/Certifications/Certifications.js
+++ src/components/KnowMore/Certifications/Certifications.js
@@ -1,20 +1,49 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+  Button,
+  Box,
+} from "@material-ui/core";
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Card from "./Card";
 import { certificates } from "../../../data";
 
 function Certifications() {
   const classes = useStyles();
+  const theme = useTheme();
+  const { t } = useTranslation();
   const [loadedCertificates, setLoadedCertificates] = useState([]);
+  const [pageNo, setPageNo] = useState(0);
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [pageSize, setPageSize] = useState(6);
+
+  function loadCertificates() {
+    const startIndex = pageNo * pageSize;
+    const endIndex = startIndex + pageSize;
+    let newCertificates = certificates
+      .sort((a, b) => b.id - a.id)
+      .filter((value, index) => {
+        return index >= startIndex && index < endIndex;
+      });
+    setLoadedCertificates((prevCertificates) => [
+      ...prevCertificates,
+      ...newCertificates,
+    ]);
+    setPageNo((prevPageNo) => prevPageNo + 1);
+  }
 
   useEffect(() => {
-    setLoadedCertificates(certificates);
+    if (isMobile) setPageSize(3);
+    loadCertificates();
   }, []);
 
   return (
     <>
       <Grid container spacing={4} className={classes.galleryContainer}>
-        {loadedCertificates.sort((a,b) => b.id - a.id).map((item, k) => (
+        {loadedCertificates.map((item, k) => (
           <Grid
             item
             xs={12}
@@ -37,6 +66,19 @@ function Certifications() {
           </Grid>
         ))}
       </Grid>
+      {loadedCertificates.length < certificates.length && (
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button
+            className={classes.loadBtn}
+            onClick={loadCertificates}
+            variant="contained"
+            color="primary"
+            projectList
+          >
+            {t("project_load_btn")}
+          </Button>
+        </Box>
+      )}
     </>
   );
 }
