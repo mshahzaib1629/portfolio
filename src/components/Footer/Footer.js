0f436e04f0bfae7a27a214fdb0b8099825a58100
Shahzaib Minhas <mshahzaib1629@gmail.com>
2022-12-11 01:46:14 +0500

    Login button added to Portfolio page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,11 +9,15 @@ import {
   useMediaQuery,
 } from "@material-ui/core";
 import Social from "../Social";
+import { useNavigate } from "react-router";
+import { Button } from "@mui/material";
 
 const Footer = () => {
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Divider style={{ backgroundColor: theme.palette.primary.main }} />
@@ -22,7 +26,7 @@ const Footer = () => {
         <Typography variant="body2" color="initial">
           © 2022 Shahzaib Minhas, Inc
           <br />
-          Designed by {" "}
+          {/* Designed by{" "}
           <a
             href="https://www.linkedin.com/in/mehdibha/"
             target={"_blank"}
@@ -30,8 +34,18 @@ const Footer = () => {
             className={classes.cite}
           >
             Mehdi BHA
-          </a>
+          </a> */}
         </Typography>
+        <Button
+          variant="text"
+          style={{ color: "white", }}
+          sx={{ ml: 0, mt: 0, mb: 0, mr: 0 }}
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Login to Portal
+        </Button>
       </Box>
     </Container>
   );
@@ -43,9 +57,9 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(6),
   },
   cite: {
-    textDecoration: 'none',
-    color: 'inherit'
-  }
+    textDecoration: "none",
+    color: "inherit",
+  },
 }));
 
 export default Footer;
