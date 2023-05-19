839e00d3f08f74981b4ae6b0240044ad071c2716
Shahzaib Minhas <shahzaib.minhas@codedistrict.com>
2023-05-19 11:31:44 +0500

    horizontal scrolling added

--- src/components/KnowMore/Certifications/Certifications.js
+++ src/components/KnowMore/Certifications/Certifications.js
@@ -1,4 +1,12 @@
-import { Grid, makeStyles, useTheme, Button, Box } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Grid,
+  makeStyles,
+  useTheme,
+  Button,
+  Box,
+  Typography,
+} from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import Card from "./Card";
 import { useSelector, useDispatch } from "react-redux";
@@ -28,29 +36,31 @@ function Certifications() {
   const viewContent = () => {
     return (
       <>
-        <Grid container spacing={4} className={classes.galleryContainer}>
-          {certificationList.map((item, k) => (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={6}
-              lg={4}
-              key={item.id}
-              classes={{ item: classes.item }}
-            >
-              <Card
-                id={item.id}
-                title={item.title}
-                issuedBy={item.issuedBy}
-                type={item.type}
-                date={item.date}
-                imageUrl={item.imageUrl}
-                url={item.url}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        <div className={classes.scrollContainer}>
+          <Grid container spacing={4} className={classes.galleryContainer}>
+            {certificationList.map((item, k) => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={6}
+                lg={4}
+                key={item.id}
+                classes={{ item: classes.item }}
+              >
+                <Card
+                  id={item.id}
+                  title={item.title}
+                  issuedBy={item.issuedBy}
+                  type={item.type}
+                  date={item.date}
+                  imageUrl={item.imageUrl}
+                  url={item.url}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
 
         <Box display="flex" justifyContent="center" mt={2}>
           <Button
@@ -69,7 +79,7 @@ function Certifications() {
   };
 
   const buildContent = () =>
-    certificationList.length == 0 ? (
+    certificationList.length === 0 ? (
       <TryAgain
         message="Unable to fetch certificates!"
         callback={getCertificationData}
@@ -83,16 +93,26 @@ function Certifications() {
 
 const useStyles = makeStyles((theme) => ({
   galleryContainer: {
-    overflow: "visible",
+    display: "flex",
+    flexWrap: "nowrap",
+    overflowX: "auto",
     width: "100%",
     margin: "0 auto",
   },
   item: {
-    overflow: "visible",
+    flex: "0 0 auto",
+    width: "400px",
+    height: "100%",
+    margin: "0 8px", // Adjust the margin as needed
+  },
+  scrollContainer: {
+    overflowX: "scroll",
+    "-webkit-overflow-scrolling": "touch",
+    scrollSnapType: "x mandatory",
   },
   loadBtn: {
     width: "200px",
-    fontSize: "20px"
+    fontSize: "20px",
   },
 }));
 
